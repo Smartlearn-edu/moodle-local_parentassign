cef9429908ace6ec2609a3682d61a761511ca9db
Smartlearn-edu <onlymade@gmail.com>
2026-03-07 02:22:01 +0200

    Remove ci directory from git tracking

--- ci/vendor/moodlehq/moodle-local_ci/tracker_automations/bulk_prelaunch_jobs/criteria/multi_database/postissue.sh
+++
@@ -1,5 +0,0 @@
-# Update the automated testing field with the results.
-${basereq} --action setFieldValue \
-    --issue ${issue} \
-    --field "Automated test results" \
-    --file "${resultfile}.${issue}.txt"
