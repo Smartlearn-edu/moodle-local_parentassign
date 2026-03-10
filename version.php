f60be38d63072f2a2c988143a727babb7a40a86e
Smat Learn <113486938+Smartlearn-edu@users.noreply.github.com>
2026-03-10 03:58:41 +0200

    Change plugin maturity from ALPHA to STABLE

--- version.php
+++ version.php
@@ -26,5 +26,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'local_parentassign';
 $plugin->version   = 2023112400;
 $plugin->requires  = 2022112800; // Moodle 4.1+.
-$plugin->maturity  = MATURITY_ALPHA;
+$plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = '1.0.0';
